9c1a9030a41c6d33494b5aad37bedea9c7303544
Jingyi Shen <131424190+CSCmaster@users.noreply.github.com>
2024-04-07 22:48:44 -0400

    Add files via upload

--- script/04_model.R
+++ script/04_model.R
@@ -7,34 +7,53 @@
 # Pre-requisites: The 'rstanarm' package for Bayesian modeling, 'readr' for data manipulation, and 'here' for path management
 # The 'cleaned_data' dataset must be pre-processed before using it in the model.
 
-
 library(rstanarm)
 library(dplyr)
-library(here)
 library(readr)
+library(here)
 
+set.seed(302)
+
+# Read in the cleaned survey data
 survey_data <- read_csv(here("data", "analysis_data", "cleaned_survey_data.csv"))
 
+# Convert the necessary columns to factors
 survey_data <- survey_data %>%
-  mutate(province = as.factor(province),
-         sex = as.factor(sex),
-         education = as.factor(education),
-         religion = as.factor(religion),
-         income_before_tax = as.factor(income_before_tax))
+  mutate(
+    province = as.factor(province),
+    sex = as.factor(sex),
+    education = as.factor(education),
+    religion = as.factor(religion),
+    income_before_tax = as.factor(income_before_tax)
+  )
 
+# Set up the priors
+prior_intercept <- normal(0, 2.5)
+prior_covariates <- normal(0, scale = 1)  # The scale can be treated as 1/sqrt(lambda) for Exponential(lambda)
 
-liberal_model <- stan_glm(vote_liberal ~ age + sex + province + education + religion + income_before_tax,
-                          data = survey_data, family = binomial(link = "logit"))
-
-
-conservative_model <- stan_glm(vote_conservative ~ age + sex + province + education + religion + income_before_tax,
-                               data = survey_data, family = binomial(link = "logit"))
-
-saveRDS(
-  liberal_model,
-  file = here("models", "liberal_model.rds")
+# Define the model using stan_glm
+liberal_model <- stan_glm(
+  vote_liberal ~ age + sex + province + education + religion + income_before_tax,
+  data = survey_data, 
+  family = binomial(link = "logit"),
+  prior_intercept = prior_intercept,
+  prior = prior_covariates,
+  prior_aux = exponential(1),
+  chains = 4,
+  iter = 2000
 )
-saveRDS(
-  conservative_model,
-  file = here("models", "conservative_model.rds")
+
+conservative_model <- stan_glm(
+  vote_conservative ~ age + sex + province + education + religion + income_before_tax,
+  data = survey_data, 
+  family = binomial(link = "logit"),
+  prior_intercept = prior_intercept,
+  prior = prior_covariates,
+  prior_aux = exponential(1),
+  chains = 4,
+  iter = 2000
 )
+
+# Save the models
+saveRDS(liberal_model, file = here("models", "liberal_model.rds"))
+saveRDS(conservative_model, file = here("models", "conservative_model.rds"))
