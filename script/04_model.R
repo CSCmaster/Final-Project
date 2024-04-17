6628769e4c77fdc333fc352f5c095ada162c4c77
Jingyi Shen <131424190+CSCmaster@users.noreply.github.com>
2024-04-16 20:59:39 -0400

    Add files via upload

--- script/04_model.R
+++ script/04_model.R
@@ -12,48 +12,29 @@ library(dplyr)
 library(readr)
 library(here)
 
-set.seed(302)
+m_lib <- glm(vote_liberal ~ age+sex+province+education+religion+income_before_tax,
+             data=cleaned_survey_data, family = "binomial")
+summary(m_lib)
 
-# Read in the cleaned survey data
-survey_data <- read_csv(here("data", "analysis_data", "cleaned_survey_data.csv"))
 
-# Convert the necessary columns to factors
-survey_data <- survey_data %>%
-  mutate(
-    province = as.factor(province),
-    sex = as.factor(sex),
-    education = as.factor(education),
-    religion = as.factor(religion),
-    income_before_tax = as.factor(income_before_tax)
-  )
+step_lib = stepAIC(m_lib, direction = "both", k = 2)
 
-# Set up the priors
-prior_intercept <- normal(0, 2.5)
-prior_covariates <- normal(0, scale = 1)  # The scale can be treated as 1/sqrt(lambda) for Exponential(lambda)
 
-# Define the model using stan_glm
-liberal_model <- stan_glm(
-  vote_liberal ~ age + sex + province + education + religion + income_before_tax,
-  data = survey_data, 
-  family = binomial(link = "logit"),
-  prior_intercept = prior_intercept,
-  prior = prior_covariates,
-  prior_aux = exponential(1),
-  chains = 4,
-  iter = 2000
-)
 
-conservative_model <- stan_glm(
-  vote_conservative ~ age + sex + province + education + religion + income_before_tax,
-  data = survey_data, 
-  family = binomial(link = "logit"),
-  prior_intercept = prior_intercept,
-  prior = prior_covariates,
-  prior_aux = exponential(1),
-  chains = 4,
-  iter = 2000
-)
+m_cons <- glm(vote_conservative ~ age+sex+province+education+religion+income_before_tax,
+              data=cleaned_survey_data, family = "binomial")
 
-# Save the models
-saveRDS(liberal_model, file = here("models", "liberal_model.rds"))
-saveRDS(conservative_model, file = here("models", "conservative_model.rds"))
+step_cons = stepAIC(m_cons, direction = "both", k = 2)
+
+saveRDS(m_lib, file = here("models", "liberal_model.rds"))
+
+
+saveRDS(m_cons, file = here("models", "conservative_model.rds"))
+
+
+
+
+
+
+
+
